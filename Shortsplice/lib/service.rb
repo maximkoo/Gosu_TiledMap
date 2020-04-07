40a4e3386c6542a7700ea1e8e8feea07d2c2b503
maximkoo <svazarm@yandex.ru>
2020-04-07 20:08:19 +0300

    Test Runner introduced

---
+++ Shortsplice/lib/service.rb
@@ -0,0 +1,33 @@
+# log levels
+LOG_LEVEL=:all
+LOG_LEVELS={:all=>0, :debug=>1, :severe=>2, :force=>3, :none=>4}
+
+V_230=2**30
+V_231=2**31
+
+DATA_X={"id"=>"1000","name"=>"Layer_X","type"=>"objectgroup","visible"=>"true","x"=>"0","y"=>"0","opacity"=>"1","objects"=>[]}
+
+class Service
+	def Service.set_data_methods(obj,data)
+		return if data.empty?
+		data.each do |k,v|
+			#puts "#{k.to_sym} #{v}"
+			obj.class.class_eval do
+				define_method("#{k.to_s}") do
+					instance_variable_get("@#{k.to_s}");
+				end;
+
+				define_method("#{k.to_s}=") do |x|
+					instance_variable_set("@#{k.to_s}", x)	
+				end;
+			end;
+			obj.send("#{k.to_sym}=",v)
+		end;
+	end;
+
+	def Service.log(str, lvl=:all)
+		if lvl>=LOG_LEVEL
+			puts str;
+		end;	
+	end;	
+end;
---
+++ TestRunner/lib/run.rb
@@ -0,0 +1,53 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+require "gosu"
+require 'pathname'
+require "../../Shortsplice/lib/tiled_map.rb"
+#require '.routine_holder.rb'
+#require './service.rb'
+require 'pry'
+
+FRAME_DELAY=16
+ANIMATION_DELAY=100
+
+class G1<Gosu::Window
+	def initialize
+		super 700,700, false  
+		p=File.join(File.dirname($0),'assets','maps')
+		abort "Incorrect path in run.rb" if !Pathname.new(p).exist?
+		@tm=TiledMap.new(p,"exp3.json")
+		@objects=@tm.all_objects
+		@animations=@tm.all_animations
+			#binding.pry
+		#puts "Objects are:"	
+		#puts @objects
+		# puts "---"
+		# puts 2147484061-V_231
+		# puts '---'
+	end;
+
+	def draw
+		@tm.draw
+	end;
+
+	def update
+		now=Gosu.milliseconds
+		if (now-@last_update_objects||=now) >= FRAME_DELAY 
+			@objects.map(&:update);
+			@last_update_objects=now;
+		end;
+		if (now-@last_update_animations||=now) >= ANIMATION_DELAY 
+			@animations.map(&:update);
+		 	@last_update_animations=now;
+		end;	
+	end;
+end;
+
+#aa=[1,2,3,4,1]
+#puts aa.collect{|x| if x==1 then x end}
+
+g1=G1.new
+g1.show;	
+
